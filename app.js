b074b8754c7720efddac3a0a92e0210a1762b5e6
seobin0224 <binsong2@sookmyung.ac.kr>
2024-08-21 18:16:05 +0900

    Feat: 토스 페이 연동 (추후 변경 예정)

--- app.js
+++ app.js
@@ -10,23 +10,24 @@ dotenv.config(); //추가
 const passport = require("./src/passport/passport");
 const connect = require("./src/config/database");
 
-const usersRouter = require("./src/routes/users");
-const commentsRouter = require("./src/routes/comments");
+const usersRouter = require("./src/routes/userRoutes");
 const postRoutes = require("./src/routes/postRoutes");
 const authRouter = require("./src/routes/auth");
-const recommendationRouter = require("./src/routes/recommendation");
 const chatRoutes = require("./src/routes/chatRoutes");
 const eventRoutes = require("./src/routes/eventRoutes");
 const productRoutes = require("./src/routes/productRoutes"); // 스토어 라우트 추가
+const paymentRoutes = require("./src/routes/paymenetRoutes");
 const hashtagRoutes = require("./src/routes/hashtagRoutes");
 const mateRoutes = require("./src/routes/mateRoutes");
-const profileRoutes = require("./src/routes/profileRoutes");
 const badgeRoutes = require("./src/routes/badgeRoutes");
 const activityMapRoutes = require("./src/routes/activityMapRoutes");
 const vendorRoutes = require("./src/routes/vendorRoutes"); //업체명 저장 DB
+const preferenceRoutes = require("./src/routes/preferenceRoutes");
+
 const app = express();
 app.set("port", process.env.PORT || 3002);
 app.set("view engine", "html");
+
 nunjucks.configure("views", {
   express: app,
   watch: true,
@@ -47,18 +48,18 @@ app.use(passport.initialize());
 
 //라우터설정 (추가)
 app.use("/users", usersRouter);
-app.use("/comments", commentsRouter); //
 app.use("/api/posts", postRoutes);
 app.use("/api/auth", authRouter);
-app.use("/api/recommendations", recommendationRouter);
 app.use("/api/chats", chatRoutes);
 app.use("/api/events", eventRoutes);
 app.use("/api/store", productRoutes); // 스토어 라우트 설정
+app.user("api/store/payments", paymentRoutes);
 app.use("/api/hashtags", hashtagRoutes);
 app.use("/api/mates", mateRoutes);
-app.use("/mypage/profile", profileRoutes);
 app.use("/mypage/badges", badgeRoutes);
 app.use("/mypage/activitymap", activityMapRoutes);
+app.use("/mypage/profile", usersRouter);
+app.use("/mypage/preference", preferenceRoutes);
 app.use("/api", vendorRoutes); // 업체명 라우트 추가
 
 //socket.io 연결
