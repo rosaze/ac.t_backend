8c96e136f181cb86a78de88917fa37397d3a69e6
seobin0224 <binsong2@sookmyung.ac.kr>
2024-08-25 15:36:44 +0900

    Feat: authorize 미들웨어 전체 적용

--- src/routes/rentalRoutes.js
+++ src/routes/rentalRoutes.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const RentalController = require('../controllers/RentalController.js');
+const authorize = require('../middleware/authorize');
 
 const router = express.Router();
 
 // 렌탈 장비 생성
-router.post('/', RentalController.createRental);
+router.post('/', authorize, RentalController.createRental);
 
 // 렌탈 장비 목록 조회
-router.get('/', RentalController.listRentals);
+router.get('/', authorize, RentalController.listRentals);
 
 // 특정 렌탈 장비 조회
-router.get('/:rentalId', RentalController.getRental);
+router.get('/:rentalId', authorize, RentalController.getRental);
 
 // 렌탈 장비 수정
-router.put('/:rentalId', RentalController.updateRental);
+router.put('/:rentalId', authorize, RentalController.updateRental);
 
 // 렌탈 장비 삭제
-router.delete('/:rentalId', RentalController.deleteRental);
+router.delete('/:rentalId', authorize, RentalController.deleteRental);
 
 module.exports = router;
