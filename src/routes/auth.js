802f5d80657dc66f231b509dc9e252d53e224e70
seobin0224 <binsong2@sookmyung.ac.kr>
2024-08-27 16:29:52 +0900

    Feat: auth 로그아웃 문제 수정중

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,29 +4,28 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 const { sendAuthNumber } = require('../passport/EmailService');
 const passport = require('passport');
-const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
+const JWT_SECRET = `${process.env.JWT_SECRET}`;
+const JWT_REFRESH_SECRET = `${process.env.JWT_REFRESH_SECRET}`;
 
 const router = express.Router();
 
+// 토큰 생성 함수들
 const generateAccessToken = (user) => {
-  const payload = {
-    id: user._id,
-    email: user.email,
-    name: user.name,
-  };
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: '30m' });
+  console.log('Access 토큰 생성 - 사용된 SECRET:', JWT_SECRET);
+  return jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, {
+    expiresIn: '1h',
+  });
 };
 
 const generateRefreshToken = (user) => {
-  const payload = {
-    id: user._id,
-    email: user.email,
-  };
-  return jwt.sign(payload, JWT_REFRESH_SECRET, { expiresIn: '7d' });
+  console.log('Refresh 토큰 생성 - 사용된 SECRET:', JWT_REFRESH_SECRET);
+  return jwt.sign({ id: user._id, email: user.email }, JWT_REFRESH_SECRET, {
+    expiresIn: '7d',
+  });
 };
 
 router.post('/register', async (req, res) => {
+  //테스트 완료
   const { email, password, phone, name } = req.body;
   try {
     const existingUser = await User.findOne({ email });
@@ -95,6 +94,7 @@ router.post('/verify', async (req, res) => {
   }
 });
 
+// 로그인 라우트
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -103,37 +103,31 @@ router.post('/login', async (req, res) => {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
-    // JWT 토큰 생성
     const accessToken = generateAccessToken(user);
     const refreshToken = generateRefreshToken(user);
 
     user.refreshToken = refreshToken;
     await user.save();
 
+    console.log('로그인 - 생성된 refreshToken:', refreshToken);
+
+    // 저장 직후 데이터베이스에서 사용자 정보 재조회
+    const updatedUser = await User.findById(user._id);
+    console.log('저장 후 DB의 refreshToken:', updatedUser.refreshToken);
+
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
-      path: '/',
-      sameSite: 'lax',
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7일
     });
 
-    res.status(201).json({ success: true, accessToken, user });
+    res.status(200).json({ success: true, accessToken });
   } catch (error) {
-    console.error('Error during login:', error.message);
-    res.status(500).json({ error: error.message });
+    console.error('로그인 에러:', error);
+    res.status(500).json({ message: 'Internal server error' });
   }
 });
-
-router.get('/kakao', passport.authenticate('kakao'));
-
-router.get(
-  '/kakao/callback',
-  passport.authenticate('kakao', {
-    failureRedirect: '/login',
-  }),
-  (req, res) => {
-    res.redirect('/');
-  }
-);
 
 router.post('/kakao/register', async (req, res) => {
   const { kakaoId, email, name, gender, age, bio } = req.body;
@@ -155,10 +149,148 @@ router.post('/kakao/register', async (req, res) => {
   }
 });
 
-router.post('/logout', (req, res) => {
-  req.logout();
-  res.clearCookie('refreshToken');
-  res.status(200).json({ message: 'Logged out successfully' });
+// 회원가입 후 추가 정보 입력 라우터 (닉네임, 한줄소개, 성별, 나이)
+router.post('/additional-info', async (req, res) => {
+  // 테스트 완료
+  const { userId, nickname, bio, gender, age } = req.body;
+
+  try {
+    // 유저 정보 찾기
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // 유저 정보 업데이트
+    user.nickname = nickname; // nickname 별도로 저장
+    user.bio = bio;
+    user.gender = gender;
+    user.age = age;
+
+    await user.save();
+
+    // 새로운 Access Token 생성
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    // 밸런스 게임 페이지로 이동
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// 밸런스 게임 페이지
+router.post('/balance-game', async (req, res) => {
+  //테스트 완료
+  const {
+    userId,
+    location_preference,
+    environment_preference,
+    group_preference,
+    season_preference,
+  } = req.body;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Update user preferences
+    user.preference = {
+      location: location_preference,
+      environment: environment_preference,
+      group: group_preference,
+      season: season_preference,
+    };
+
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    res.status(200).json({ token, user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Refresh Token으로 새로운 Access Token 발급
+router.post('/token', async (req, res) => {
+  const { refreshToken } = req.cookies; // 쿠키에서 Refresh Token 가져오기
+
+  if (!refreshToken) {
+    return res.status(403).json({ message: 'Refresh token is required' });
+  }
+
+  try {
+    console.log('Received refreshToken:', refreshToken);
+
+    // Mongoose를 사용하여 refresh token으로 사용자를 찾음
+    const user = await User.findOne({ refreshToken });
+
+    if (!user) {
+      console.log('No user found with provided refresh token');
+      return res.status(403).json({ message: 'Invalid refresh token' });
+    }
+
+    jwt.verify(refreshToken, JWT_REFRESH_SECRET, async (err, decoded) => {
+      if (err) {
+        console.log('JWT verification failed:', err.message);
+        return res.status(403).json({ message: 'Invalid refresh token' });
+      }
+
+      const newAccessToken = generateAccessToken(user);
+
+      res.status(200).json({ accessToken: newAccessToken });
+    });
+  } catch (error) {
+    console.error('Error during token processing:', error.message);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// 로그아웃 라우트
+router.post('/logout', async (req, res) => {
+  const { refreshToken } = req.cookies;
+
+  if (!refreshToken) {
+    console.log('로그아웃 요청 - refreshToken 없음');
+    return res.status(400).json({ message: 'Refresh token is required' });
+  }
+
+  try {
+    console.log('로그아웃 요청 - 받은 refreshToken:', refreshToken);
+
+    // JWT 검증
+    jwt.verify(refreshToken, JWT_REFRESH_SECRET, async (err, decoded) => {
+      if (err) {
+        console.log('JWT 검증 실패:', err.message);
+        return res.status(403).json({ message: 'Invalid refresh token' });
+      }
+
+      const user = await User.findOne({ refreshToken });
+
+      if (!user) {
+        console.log('해당 refreshToken을 가진 사용자 없음');
+        return res.status(403).json({ message: 'Invalid refresh token' });
+      }
+
+      console.log('찾은 사용자의 refreshToken:', user.refreshToken);
+
+      user.refreshToken = null;
+      await user.save();
+
+      res.clearCookie('refreshToken');
+      res.status(200).json({ message: 'Logged out successfully' });
+    });
+  } catch (error) {
+    console.error('로그아웃 에러:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 });
 
 module.exports = router;
