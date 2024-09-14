edccf1dbd11a3edf8a1ebe39a9733a69d3273b45
rosaze <geenieeyoon@gmail.com>
2024-09-14 17:19:42 +0900

    Feat: 숙박 서비스, 숙박 모델

---
+++ src/models/Accommodation.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const accommodationSchema = new mongoose.Schema({
+  sigungu: String,
+  title: String,
+  category2: String,
+  category3: String,
+  // 기타 필요한 필드들...
+});
+
+const Accommodation = mongoose.model('Accommodation', accommodationSchema);
+
+module.exports = Accommodation;
--- src/services/AccommodationService.js
+++ src/services/AccommodationService.js
@@ -1,18 +1,3 @@
-// models/Accommodation.js
-const mongoose = require('mongoose');
-
-const accommodationSchema = new mongoose.Schema({
-  sigungu: String,
-  title: String,
-  category2: String,
-  category3: String,
-  // 기타 필요한 필드들...
-});
-
-const Accommodation = mongoose.model('Accommodation', accommodationSchema);
-
-module.exports = Accommodation;
-
 // services/AccommodationService.js
 const Accommodation = require('../models/Accommodation');
 
