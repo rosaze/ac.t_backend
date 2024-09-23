d888aeefb66e84c9300887994aa5c587678b1bb4
rosaze <geenieeyoon@gmail.com>
2024-09-23 20:48:51 +0900

    feat: 1차시도: 로컬주소 -> actflaskapi

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 const apiClient = axios.create({
-  baseURL: 'http://127.0.0.1:5003/api/v1', // Flask API의 기본 URL
+  baseURL: 'http://actflaskapi:5003', // Flask API의 기본 URL
   timeout: 5000,
 });
 
